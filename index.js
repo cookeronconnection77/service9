ec591a567821d297154c8309369bd98c33af7e17
cookeronconnection77 <cookeronconnection77@gmail.com>
2025-02-12 16:56:10 -0500

    arreglando bug

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 let mesas = {};  
 const fs = require("node:fs")
 
-const frontendUrl = "http://localhost:3000";
+const frontendUrl = "http://localhost:3000/service1/";
 
 
 // app.use(multer({storage: storage, dest: path.join(__dirname, "./public/images")}).single("image"))
